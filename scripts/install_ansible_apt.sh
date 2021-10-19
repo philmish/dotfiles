d93926419aa27044fa15f600abd86b3c60c7c2f4
philmish <thantalosdream@protonmail.com>
2021-10-19 04:22:57 +0200

    Fixed ansible install script

--- scripts/install_ansible_apt.sh
+++ scripts/install_ansible_apt.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/bash
 
-<<<<<<< HEAD
 apt-add-repository --yes --update ppa:ansible/ansible && apt install ansible;
-=======
-apt-add-repository --yes --update ppa:ansible/ansible && apt update && apt install ansible;
->>>>>>> f6081c142414fdcc12889d4901e240a1ce495a84
