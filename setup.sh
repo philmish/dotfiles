604570dfbb354fd12765225bb6284ff33c11de91
philmish <thantalosdream@protonmail.com>
2022-08-31 23:42:31 +0200

    feat: add os determination method to setup script

--- setup.sh
+++ setup.sh
@@ -4,8 +4,22 @@
 
 DIRS=(/code/repos /code/poc /code/work /Wallpaper /fonts)
 
-
 # UTILS
+
+get_package_manager() {
+    declare -A osInfo;
+    osInfo[/etc/arch-release]=pacman
+    osInfo[/etc/debian_version]=apt
+    osInfo[/etc/alpine-release]=apk
+    osInfo[/etc/redhat-release]=yum
+
+    for f in ${!osInfo[@]}
+    do
+        if [[ -f $f ]]; then
+            PKG=${osInfo[$f]}
+        fi
+    done
+}
 
 create_dirs() {
     for i in "${DIRS[@]}"
