cdffca5574206a8c4da8ceab4e252960436f2c11
philmish <thantalosdream@protonmail.com>
2022-09-13 01:21:53 +0200

    feat: add more go snippets for neovim

--- nvim/.config/nvim/after/plugin/snippets/go.lua
+++ nvim/.config/nvim/after/plugin/snippets/go.lua
@@ -1,6 +1,8 @@
 local ls = require "luasnip"
 local s = ls.s
 local inode = ls.insert_node
+local cnode = ls.choice_node
+local tnode = ls.text_node
 local fmt = require("luasnip.extras.fmt").fmt
 
 ls.add_snippets(nil, {
@@ -12,6 +14,41 @@ ls.add_snippets(nil, {
             }}
             {}]],
             { inode(0) }
+        )),
+        s("strt", fmt(
+            [[ 
+            type {} struct {{
+                {}
+            }}
+            ]],
+            { inode(1), inode(0) }
+        )),
+        s("fn", fmt(
+        [[ 
+        func {}({}) ({}) {{
+            {}
+        }}
+        ]],
+        { 
+            inode(1),
+            cnode(2, { inode(nil, "params"), tnode"" }),
+            cnode(3, { inode(nil, "returns"), tnode"" }),
+            inode(0)
+        }
+        )),
+        s("main", fmt(
+        [[ 
+        package main
+
+        import (
+            "{}"
+        )
+
+        func main() {
+            {}
+        }
+        ]],
+        { inode(1), inode(0) }
         )),
     }
 })
