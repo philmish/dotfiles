ed0f5ffec92d5b31a64696bfa46375625472a68d
philmish <thantalosdream@protonmail.com>
2023-04-12 00:37:24 +0200

    refactor: use mapping to load driver for plugin dynamically

--- nvim/.config/nvim/lua/td/init.lua
+++ nvim/.config/nvim/lua/td/init.lua
@@ -3,20 +3,19 @@ local js_d = require("td.js_driver")
 local php_d = require("td.php_driver")
 local py_d = require("td.python_driver")
 
-local M = {}
-
-M._curr_ft = nil
-M._loaded_driver = nil
-
-M.drivers = {
-    ["go"] = go_d,
-    ["javascript"] = js_d,
-    ["php"] = php_d,
-    ["python"] = py_d,
+local M = {
+    _curr_ft = nil,
+    _loaded_driver = nil,
+    drivers = {
+        ["go"] = go_d,
+        ["javascript"] = js_d,
+        ["php"] = php_d,
+        ["python"] = py_d,
+    }
 }
 
 M.load_driver = function (lang)
-    M._loaded_driver = M.drivers["javascript"]
+    M._loaded_driver = M.drivers[lang]
     if M._loaded_driver == nil then
         print("Found no driver for current file type ", M._curr_ft)
     end
@@ -26,19 +25,9 @@ M._load_ft = function ()
     M._curr_ft = vim.bo.filetype
 end
 
-M._detect_ft = function ()
-    if vim.bo.filetype == "php" then
-        print("php")
-    elseif vim.bo.filetype == "javascript" then
-        print("javascript")
-    else
-        print("Unknow ft ", vim.bo.filetype)
-    end
-end
-
 M.run_tests = function ()
     M._load_ft()
-    M.load_driver()
+    M.load_driver(M._curr_ft)
     if M._loaded_driver == nil then
         print("No driver for current file type ", M._curr_ft)
         return
