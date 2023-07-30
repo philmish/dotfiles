0d7f32f7059a32018e36f2c840448b89b9d468ce
philmish <thantalosdream@protonmail.com>
2023-07-30 13:13:59 +0200

    improve: enable treesitter autoindent

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -2,7 +2,9 @@ require("nvim-treesitter.configs").setup {
     ensure_installed = {"vimdoc", "php", "c", "go", "rust", "bash", "javascript", "typescript", "lua"},
     sync_install = false,
     auto_install = true,
-
+    indent = {
+        enable = true
+    },
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
