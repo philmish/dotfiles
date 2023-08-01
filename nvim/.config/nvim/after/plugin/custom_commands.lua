df8693d90f42bc4b465283cd35dd174a2f5d68f6
philmish <thantalosdream@protonmail.com>
2023-08-01 22:52:25 +0200

    feat: add user command to run cs fixer

--- nvim/.config/nvim/after/plugin/custom_commands.lua
+++ nvim/.config/nvim/after/plugin/custom_commands.lua
@@ -24,3 +24,30 @@ vim.api.nvim_create_user_command("BgTransparent", function ()
     vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
 end, {})
 
+-- Run CS-Fixer with provided config
+vim.api.nvim_create_user_command("CSFix", function (opts)
+
+    local config_path = opts.fargs[1]
+    local cmd = "./vendor/bin/php-cs-fixer fix --config " .. config_path
+    vim.notify("Running: " .. cmd, vim.log.levels.INFO, {})
+
+    vim.fn.jobstart(cmd, {
+        on_stderr = function (_, data)
+            if not data then
+                return
+            end
+            for _, line in ipairs(data) do
+                pcall(function ()
+                    vim.notify("CS-Fixer crashed:  " .. line, vim.log.levels.ERROR, {})
+                end)
+            end
+        end,
+        on_exit = function ()
+            vim.cmd "edit"
+            vim.notify("CS Fixer finished running", vim.log.levels.INFO, {
+                title = "Php-CS-Fixer",
+                timeout = 500
+            })
+        end
+    })
+end, { nargs = 1 })
