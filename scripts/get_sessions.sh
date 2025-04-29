563fd52a509bd3e3fdc7f510d574212bb0915f1a
philmish <philmish.dev@gmail.com>
2025-04-29 21:20:52 +0000

    improve: tmux session list script with guard and early exit

--- scripts/get_sessions.sh
+++ scripts/get_sessions.sh
@@ -6,6 +6,11 @@ interup () {
 
 trap interup INT
 
+if [[ -z $(pgrep tmux) ]]; then
+    echo "Tmux Server is not running, no sessions to connect to."
+    exit 1
+fi
+
 selected=$(tmux list-sessions | fzf)
 name=$(echo "$selected" | awk -F: '{print $1}')
 `tmux attach-session -t "$name"`
