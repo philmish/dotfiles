71c839a13e7d60f0cd0d636513b282e8e50c76aa
philmish <philmish.dev@gmail.com>
2025-05-03 14:39:01 +0000

    fix: check for empty inputs in tmux session create and get scripts

--- scripts/create_session.sh
+++ scripts/create_session.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/bash
 
 selected=$(find ~/projects/public ~/projects/private  ~/.config -mindepth 1 -maxdepth 1 -type d | fzf)
+
+if [[ -z $selected ]]; then
+    echo "No directory selected to create a session from."
+    exit 1
+fi
+
 name=$(echo "$selected" | rev | cut -d "/" -f1 | rev | tr . _)
-
 tmux_running=$(pgrep tmux)
-
 if [[ -z tmux_running ]]; then
     tmux new-session -s $name -c $selected
     exit 0
--- scripts/get_sessions.sh
+++ scripts/get_sessions.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/bash
 
-interup () {
-    exit 1
-}
-
-trap interup INT
-
 if [[ -z $(pgrep tmux) ]]; then
     echo "Tmux Server is not running, no sessions to connect to."
     exit 1
 fi
 
 selected=$(tmux list-sessions | fzf)
+
+if [[ -z $selected ]]; then
+    echo "No session to open selected."
+    exit 1
+fi
 name=$(echo "$selected" | awk -F: '{print $1}')
 tmux attach-session -t "$name"
