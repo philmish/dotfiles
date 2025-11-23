05c9026df1733e915e14704626c33aa527f2a945
philmish <philmish.dev@gmail.com>
2025-11-23 13:20:21 +0100

    refactor: dont auto activate gruvbox

--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -3,8 +3,8 @@ return {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
     config = function()
-      vim.opt.background = "dark"
-      vim.cmd([[colorscheme gruvbox]])
+      -- vim.opt.background = "dark"
+      -- vim.cmd([[colorscheme gruvbox]])
     end,
   },
 }
