f4c79ffa8514278c8be7b1d4d0538a864934edf6
philmish <philmish.dev@gmail.com>
2025-11-23 13:18:53 +0100

    feat: add pre write autocmd to format json files with jq

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,10 +1,28 @@
 local api = vim.api
 
 local ft_set_group = api.nvim_create_augroup('set_ft', { clear = true })
+local pre_write_fmt_group = api.nvim_create_augroup('pre_write_fmt', { clear = true })
 
 -- set the filetype for terraform related files
 api.nvim_create_autocmd(
   { "BufRead", "BufNewFile" },
   { group = ft_set_group, pattern = {"*.tf", "*.tfvars"}, command = "set filetype=terraform" }
+)
+
+-- use jq to format json files
+api.nvim_create_autocmd(
+  {"BufWritePre"},
+  {
+    group = pre_write_fmt_group,
+    pattern = {"*.json"},
+    callback = function ()
+      local save_cursor = vim.fn.winsaveview()
+      -- use pcall to avoid crash if jq is not installed
+      pcall(function ()
+        vim.cmd([[ :%! jq --indent 2 . ]])
+      end)
+      vim.fn.winrestview(save_cursor)
+    end,
+  }
 )
 
