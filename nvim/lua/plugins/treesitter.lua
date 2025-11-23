62702a894892ea7896a326ddffa3a3411ec001c0
philmish <philmish.dev@gmail.com>
2025-11-23 13:21:55 +0100

    refactor: treesitter plugin config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,18 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    build = function()
-      require("nvim-treesitter.install").update({ with_sync = true })()
+    lazy = false,
+    build = ":TSUpdate",
+    config = function ()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "c", "lua", "rust", "go", "javascript", "json", "yaml",
+          "zig", "helm", "html", "sql", "css", "hcl", "luadoc",
+          "make", "nginx", "php", "toml", "terraform", "typescript",
+        },
+        sync_install = true,
+        auto_install = false,
+      })
     end,
   },
 }
