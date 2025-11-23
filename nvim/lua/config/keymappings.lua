3e1c9520baaa9e0801640071a89c54e174747760
philmish <philmish.dev@gmail.com>
2025-11-23 13:20:10 +0100

    feat: add everforest colorschema

--- nvim/lua/config/keymappings.lua
+++ nvim/lua/config/keymappings.lua
@@ -1,7 +1,12 @@
+-- open file explorer
 vim.keymap.set("n", "<leader>e", ":Explore<CR>", { noremap = true })
+
+-- open docs for symbol in float
 vim.keymap.set("n", "K", function()
   vim.lsp.buf.hover({
     border = "rounded",
   })
 end)
+
+-- show diagnostic infos for current line in float
 vim.keymap.set("n", "<leader>d", ":lua vim.diagnostic.open_float()<CR>", { noremap = true })
