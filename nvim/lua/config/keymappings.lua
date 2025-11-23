22a5215b3d34211fedd98c736eebbb43715d2c7a
philmish <philmish.dev@gmail.com>
2025-11-23 14:54:49 +0100

    improve: add keymaps for resizing windows and sourcing current file in nvim

--- nvim/lua/config/keymappings.lua
+++ nvim/lua/config/keymappings.lua
@@ -1,12 +1,27 @@
+local mapkey = vim.keymap.set
+
 -- open file explorer
-vim.keymap.set("n", "<leader>e", ":Explore<CR>", { noremap = true })
+mapkey("n", "<leader>e", ":Explore<CR>", { noremap = true })
 
 -- open docs for symbol in float
-vim.keymap.set("n", "K", function()
+mapkey("n", "K", function()
   vim.lsp.buf.hover({
     border = "rounded",
   })
 end)
 
 -- show diagnostic infos for current line in float
-vim.keymap.set("n", "<leader>d", ":lua vim.diagnostic.open_float()<CR>", { noremap = true })
+mapkey("n", "<leader>d", vim.diagnostic.open_float, { noremap = true })
+
+-- clear search higlighting
+mapkey({ "i", "n" }, "<esc>", "<cmd>noh<CR><esc>")
+
+-- resize windows
+local opts = { noremap = true, silent = true }
+mapkey("n", "<C-S-Up>", "<cmd>resize +5<CR>", opts)
+mapkey("n", "<C-S-Down>", "<cmd>resize -5<CR>", opts)
+mapkey("n", "<C-S-Left>", "<cmd>vertical resize -5<CR>", opts)
+mapkey("n", "<C-S-Right>", "<cmd>vertical resize +5<CR>", opts)
+
+-- source current file
+mapkey("n", "<leader><leader>x", "<cmd>source %<CR>", opts)
